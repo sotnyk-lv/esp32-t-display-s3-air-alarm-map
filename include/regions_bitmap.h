9bfff1c62b410d1e383b3915f9515fa24548f8a6
sotnyk-lv <danylo.sahaidak@gmail.com>
2023-01-16 00:28:27 +0200

    added add oblasts to the map

--- include/regions_bitmap.h
+++ include/regions_bitmap.h
@@ -1,12 +1,32 @@
 #ifndef REGIONS_BITMAP_H
 #define REGIONS_BITMAP_H
 
-// #define BIT8_WHITE 0xFF
-// #define BIT8_BLACK 0x00
-// #define BIT8_RED   0xE0
+// #include "regions/ukraine.h"
 
-#include "cherkasy.h"
-#include "chernihiv.h"
-#include "ukraine.h"
+#include "regions/cherkasy.h"
+#include "regions/chernihiv.h"
+#include "regions/chernivtsi.h"
+#include "regions/crimea.h"
+#include "regions/dnipropetrovsk.h"
+#include "regions/donetsk.h"
+#include "regions/frankivsk.h"
+#include "regions/kharkiv.h"
+#include "regions/kherson.h"
+#include "regions/khmel.h"
+#include "regions/kirovohrad.h"
+#include "regions/kyiv.h"
+#include "regions/luhansk.h"
+#include "regions/lviv.h"
+#include "regions/mykolaiv.h"
+#include "regions/odesa.h"
+#include "regions/poltava.h"
+#include "regions/rivne.h"
+#include "regions/symu.h"
+#include "regions/ternopil.h"
+#include "regions/vinnytsia.h"
+#include "regions/volyn.h"
+#include "regions/zakarpattia.h"
+#include "regions/zaporizhia.h"
+#include "regions/zhytomyr.h"
 
 #endif // REGIONS_BITMAP_H
--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,13 @@ void setup() {
   lcd.setSwapBytes(true);
   lcd.fillScreen(TFT_BLACK);
 
-  // create full map
-  TFT_eSprite ukraineSprite(&lcd);
-  ukraineSprite.createSprite(254, 170);
-  ukraineSprite.setSwapBytes(true);
-  ukraineSprite.pushImage(0, 0, 254, 170, ukraineBitmap);
-  ukraineSprite.pushSprite(0, 0, TFT_BLACK);
-  ukraineSprite.deleteSprite();
+  // // create full map
+  // TFT_eSprite ukraineSprite(&lcd);
+  // ukraineSprite.createSprite(254, 170);
+  // ukraineSprite.setSwapBytes(true);
+  // ukraineSprite.pushImage(0, 0, 254, 170, ukraineBitmap);
+  // ukraineSprite.pushSprite(0, 0, TFT_BLACK);
+  // ukraineSprite.deleteSprite();
 
   delay(1000);
   
@@ -35,9 +35,55 @@ void loop() {
   delay(1000);
   cherkasy.raiseAlert();
   chernihiv.raiseAlert();
+  chernivtsi.raiseAlert();
+  crimea.raiseAlert();
+  dnipropetrovsk.raiseAlert();
+  donetsk.raiseAlert();
+  frankivsk.raiseAlert();
+  kharkiv.raiseAlert();
+  kherson.raiseAlert();
+  khmel.raiseAlert();
+  kirovohrad.raiseAlert();
+  kyiv.raiseAlert();
+  luhansk.raiseAlert();
+  lviv.raiseAlert();
+  mykolaiv.raiseAlert();
+  odesa.raiseAlert();
+  poltava.raiseAlert();
+  rivne.raiseAlert();
+  symu.raiseAlert();
+  ternopil.raiseAlert();
+  vinnytsia.raiseAlert();
+  volyn.raiseAlert();
+  zakarpattia.raiseAlert();
+  zaporizhia.raiseAlert();
+  zhytomyr.raiseAlert();
+
   delay(1000);
   cherkasy.removeAlert();
   chernihiv.removeAlert();
+  chernivtsi.removeAlert();
+  crimea.removeAlert();
+  dnipropetrovsk.removeAlert();
+  donetsk.removeAlert();
+  frankivsk.removeAlert();
+  kharkiv.removeAlert();
+  kherson.removeAlert();
+  khmel.removeAlert();
+  kirovohrad.removeAlert();
+  kyiv.removeAlert();
+  luhansk.removeAlert();
+  lviv.removeAlert();
+  mykolaiv.removeAlert();
+  odesa.removeAlert();
+  poltava.removeAlert();
+  rivne.removeAlert();
+  symu.removeAlert();
+  ternopil.removeAlert();
+  vinnytsia.removeAlert();
+  volyn.removeAlert();
+  zakarpattia.removeAlert();
+  zaporizhia.removeAlert();
+  zhytomyr.removeAlert();
 
-  
 }
