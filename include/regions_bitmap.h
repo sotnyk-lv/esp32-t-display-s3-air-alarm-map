e8e855340953bc72b1149800b50cfc1d3aaf1d94
sotnyk-lv <danylo.sahaidak@gmail.com>
2023-02-18 00:38:49 +0200

    renamed khmelnytskyi, sumy, zaporizhzhia regions
    add list or regions objects

--- include/regions_bitmap.h
+++ include/regions_bitmap.h
@@ -3,30 +3,84 @@
 
 // #include "regions/ukraine.h"
 
-#include "regions/cherkasy.h"
-#include "regions/chernihiv.h"
-#include "regions/chernivtsi.h"
-#include "regions/crimea.h"
-#include "regions/dnipropetrovsk.h"
-#include "regions/donetsk.h"
-#include "regions/frankivsk.h"
-#include "regions/kharkiv.h"
-#include "regions/kherson.h"
-#include "regions/khmel.h"
-#include "regions/kirovohrad.h"
-#include "regions/kyiv.h"
-#include "regions/luhansk.h"
-#include "regions/lviv.h"
-#include "regions/mykolaiv.h"
-#include "regions/odesa.h"
-#include "regions/poltava.h"
-#include "regions/rivne.h"
-#include "regions/symu.h"
-#include "regions/ternopil.h"
-#include "regions/vinnytsia.h"
-#include "regions/volyn.h"
-#include "regions/zakarpattia.h"
-#include "regions/zaporizhia.h"
-#include "regions/zhytomyr.h"
+#include "regions/vinnytsia.h"          //1
+#include "regions/volyn.h"              //2
+#include "regions/dnipropetrovsk.h"     //3
+#include "regions/donetsk.h"            //4
+#include "regions/zhytomyr.h"           //5
+#include "regions/zakarpattia.h"        //6
+#include "regions/zaporizhzhia.h"       //7
+#include "regions/frankivsk.h"          //8
+#include "regions/kyiv.h"               //9
+#include "regions/kirovohrad.h"         //10
+#include "regions/luhansk.h"            //11
+#include "regions/lviv.h"               //12
+#include "regions/mykolaiv.h"           //13
+#include "regions/odesa.h"              //14
+#include "regions/poltava.h"            //15
+#include "regions/rivne.h"              //16
+#include "regions/sumy.h"               //17
+#include "regions/ternopil.h"           //18
+#include "regions/kharkiv.h"            //19
+#include "regions/kherson.h"            //20
+#include "regions/khmelnytskyi.h"       //21
+#include "regions/cherkasy.h"           //22
+#include "regions/chernivtsi.h"         //23
+#include "regions/chernihiv.h"          //24
+
+#include "regions/crimea.h"             //0
+
+Region *regions[] =    {&crimea,
+                        &vinnytsia,
+                        &volyn,
+                        &dnipropetrovsk,
+                        &donetsk,
+                        &zhytomyr,
+                        &zakarpattia,
+                        &zaporizhzhia,
+                        &frankivsk,
+                        &kyiv,
+                        &kirovohrad,
+                        &luhansk,
+                        &lviv,
+                        &mykolaiv,
+                        &odesa,
+                        &poltava,
+                        &rivne,
+                        &sumy,
+                        &ternopil,
+                        &kharkiv,
+                        &kherson,
+                        &khmelnytskyi,
+                        &cherkasy,
+                        &chernivtsi,
+                        &chernihiv};
+
+
+			// {1, "Вінницька область", "Vinnytsia oblast", false, nil},
+			// {2, "Волинська область", "Volyn oblast", false, nil},
+			// {3, "Дніпропетровська область", "Dnipropetrovsk oblast", false, nil},
+			// {4, "Донецька область", "Donetsk oblast", false, nil},
+			// {5, "Житомирська область", "Zhytomyr oblast", false, nil},
+			// {6, "Закарпатська область", "Zakarpattia oblast", false, nil},
+			// {7, "Запорізька область", "Zaporizhzhia oblast", false, nil},
+			// {8, "Івано-Франківська область", "Ivano-Frankivsk oblast", false, nil},
+			// {9, "Київська область", "Kyiv oblast", false, nil},
+			// {10, "Кіровоградська область", "Kirovohrad oblast", false, nil},
+			// {11, "Луганська область", "Luhansk oblast", false, nil},
+			// {12, "Львівська область", "Lviv oblast", false, nil},
+			// {13, "Миколаївська область", "Mykolaiv oblast", false, nil},
+			// {14, "Одеська область", "Odesa oblast", false, nil},
+			// {15, "Полтавська область", "Poltava oblast", false, nil},
+			// {16, "Рівненська область", "Rivne oblast", false, nil},
+			// {17, "Сумська область", "Sumy oblast", false, nil},
+			// {18, "Тернопільська область", "Ternopil oblast", false, nil},
+			// {19, "Харківська область", "Kharkiv oblast", false, nil},
+			// {20, "Херсонська область", "Kherson oblast", false, nil},
+			// {21, "Хмельницька область", "Khmelnytskyi oblast", false, nil},
+			// {22, "Черкаська область", "Cherkasy oblast", false, nil},
+			// {23, "Чернівецька область", "Chernivtsi oblast", false, nil},
+			// {24, "Чернігівська область", "Chernihiv oblast", false, nil},
+			// {25, "м. Київ", "Kyiv", false, nil},
 
 #endif // REGIONS_BITMAP_H
--- src/main.cpp
+++ src/main.cpp
@@ -33,57 +33,13 @@ void loop() {
   Serial.println(i);
 
   delay(1000);
-  cherkasy.raiseAlert();
-  chernihiv.raiseAlert();
-  chernivtsi.raiseAlert();
-  crimea.raiseAlert();
-  dnipropetrovsk.raiseAlert();
-  donetsk.raiseAlert();
-  frankivsk.raiseAlert();
-  kharkiv.raiseAlert();
-  kherson.raiseAlert();
-  khmel.raiseAlert();
-  kirovohrad.raiseAlert();
-  kyiv.raiseAlert();
-  luhansk.raiseAlert();
-  lviv.raiseAlert();
-  mykolaiv.raiseAlert();
-  odesa.raiseAlert();
-  poltava.raiseAlert();
-  rivne.raiseAlert();
-  symu.raiseAlert();
-  ternopil.raiseAlert();
-  vinnytsia.raiseAlert();
-  volyn.raiseAlert();
-  zakarpattia.raiseAlert();
-  zaporizhia.raiseAlert();
-  zhytomyr.raiseAlert();
+  for (int i=0; i<25; ++i) {
+    regions[i]->raiseAlert();
+  }
 
   delay(1000);
-  cherkasy.removeAlert();
-  chernihiv.removeAlert();
-  chernivtsi.removeAlert();
-  crimea.removeAlert();
-  dnipropetrovsk.removeAlert();
-  donetsk.removeAlert();
-  frankivsk.removeAlert();
-  kharkiv.removeAlert();
-  kherson.removeAlert();
-  khmel.removeAlert();
-  kirovohrad.removeAlert();
-  kyiv.removeAlert();
-  luhansk.removeAlert();
-  lviv.removeAlert();
-  mykolaiv.removeAlert();
-  odesa.removeAlert();
-  poltava.removeAlert();
-  rivne.removeAlert();
-  symu.removeAlert();
-  ternopil.removeAlert();
-  vinnytsia.removeAlert();
-  volyn.removeAlert();
-  zakarpattia.removeAlert();
-  zaporizhia.removeAlert();
-  zhytomyr.removeAlert();
+  for (int i=0; i<25; ++i) {
+    regions[i]->removeAlert();
+  }
 
 }
